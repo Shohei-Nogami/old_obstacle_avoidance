5a91dc88056a6c61921109b2ce997883ff68aa52
Shohei-Nogami <14ej081@ms.dendai.ac.jp>
2018-04-13 13:53:16 +0000

    update

--- src/grid_class.cpp
+++ src/grid_class.cpp
@@ -1,12 +1,13 @@
 #include"grid_class.h"
 
 grid_class::grid_class()
-	::it(nh_pub),grid_resolution(100),grid_size(5.0)
+	:it_pub(nh_pub),grid_resolution(200),grid_size(8.0)
 {
-	pub=it.advertise("grid_image",1);
+	pub=it_pub.advertise("grid_image",1);
 	nh_sub.setCallbackQueue(&queue);
-	sub=nh_sub.subscribe("/zed/point_cloud/cloud_registered",1,&grid_class::plc_callback,this);
-	cv::Mat m = cv::Mat::zeros(cv::Size(grid_resolution,grid_resolution), CV_8UC1);
+//	sub=nh_sub.subscribe("/zed/point_cloud/cloud_registered",1,&grid_class::plc_callback,this);
+	sub=nh_sub.subscribe("/zed/depth/depth_registered",1,&grid_class::image_callback,this);
+	cv::Mat m = cv::Mat::zeros(cv::Size(grid_resolution,grid_resolution), CV_8UC3);
 	grid_map=m.clone();
 	grid_cell_size=grid_size/grid_resolution;
 }
@@ -14,6 +15,10 @@ grid_class::grid_class()
 grid_class::~grid_class(){
 
 }
+void grid_class::subscribe_depth_image(void){
+	queue.callOne(ros::WallDuration(1));
+}
+/*
 void grid_class::subscribe_pcl(void){
 	queue.callOne(ros::WallDuration(1));
 }
@@ -21,13 +26,102 @@ void grid_class::pcl_callback(const sensor_msgs::PointCloud2ConstPtr& msg){
   pcl::fromROSMsg (*msg, pcl_data);
 	ROS_INFO("into plc_callback");
 }
+*/
+void grid_class::image_callback(const sensor_msgs::ImageConstPtr& msg)
+{
+	try{
+//		std::cout<<"depth_image_callback \n";
+		cvbridge_image=cv_bridge::toCvCopy(msg,sensor_msgs::image_encodings::TYPE_32FC1);
+	}
+  catch(cv_bridge::Exception& e) {
+	std::cout<<"depth_image_callback Error \n";
+	  ROS_ERROR("Could not convert from '%s' to 'TYPE_32FC1'.",
+	  msg->encoding.c_str());
+	  return ;
+  }
+}
 void grid_class::set_grid_map(void){
-	
+	float x,y;
+	int grid_x,grid_z;
+	float depth_temp;
+	const float floor_threshold=0.10;
+	depth_image=cvbridge_image->image.clone();
+//renew
+	cv::Mat m = cv::Mat::zeros(cv::Size(grid_resolution,grid_resolution), CV_8UC3);
+	grid_map=m.clone();
+
+//	std::cout<<"before for\n";
+	for(int h=0;h<height;h++){
+		for(int w=0;w<width;w++){
+			depth_temp=depth_image.at<float>(h,w);
+//			std::cout<<"("<<h<<","<<w<<")\n";
+			if(!std::isnan(depth_temp)&&!std::isinf(depth_temp)){
+//				std::cout<<"depth_temp:"<<depth_temp<<"\n";
+				y=(height/2-h)*depth_temp/f;
+				if(y+0.23>floor_threshold){//||y_init+0.23>1.0){
+					if(depth_temp<grid_size/2){
+	//					std::cout<<"depth_temp<grid_size/2\n";
+	//					std::cout<<"depth_temp/grid_cell_size:"<<(int)(depth_temp/grid_cell_size)<<"\n";
+	//					std::cout<<"grid_resolution/2:"<<grid_resolution/2<<"\n";
+	//					std::cout<<"grid_resolution/2-(int)(depth_temp/grid_cell_size):"<<grid_resolution/2-(int)(depth_temp/grid_cell_size)<<"\n";
+//						grid_z=grid_resolution/2-(int)(depth_temp/grid_cell_size);
+							grid_z=(int)( (grid_size/2-depth_temp) /grid_cell_size);
+	//					std::cout<<"grid:(z,x):("<<grid_z;
+	//					std::cout<<"a1:\n";
+						x=(w-width/2)*depth_temp/f;
+	//					std::cout<<"a2\n";
+						if(std::abs(x)<grid_size/2){
+//							grid_x=(int)(x/grid_cell_size)+grid_resolution/2;
+							grid_x=(int)( (x+grid_size/2)/grid_cell_size);
+	//						std::cout<<","<<grid_x;
+//							grid_map.at<cv::Vec3b>(grid_z,grid_x)[1]+=2;
+							grid_map.at<cv::Vec3b>(grid_z,grid_x)[1]=255;
+//							if(grid_map.at<cv::Vec3b>(grid_z,grid_x)[1]>=255){
+//								grid_map.at<cv::Vec3b>(grid_z,grid_x)[1]=255;
+//							}
+	//						std::cout<<")\n";
+						}
+					}
+				}
+			}
+		}
+	}
+/*
+	for(int h=0;h<grid_resolution;h++){
+		for(int w=0;w<grid_resolution;w++){
+			grid_map.at<int>(h,w)-=1;				
+			if(grid_map.at<int>(h,w)<=0){
+				grid_map.at<int>(h,w)=0;
+			}
+		}
+	}
+*/
+	grid_map-=5;
+
 }
 
 void grid_class::publish_grid_map(void){
+
+	float robot_r=0.20;
+	int robot_cell_size=(int)(robot_r/grid_cell_size);
+	for(int h=grid_resolution/2-robot_cell_size/2;h<grid_resolution/2+robot_cell_size/2;h++){
+		for(int w=grid_resolution/2-robot_cell_size/2;w<grid_resolution/2+robot_cell_size/2;w++){
+			grid_map.at<cv::Vec3b>(h,w)[2]=255;			
+		}
+	}
+
+//	std::cout<<"into pulish func\n";
+/*	for(int h=0;h<grid_resolution;h++){
+		for(int w=0;w<grid_resolution;w++){
+//			std::cout<<"("<<h<<","<<w<<")\n";
+			grid_map.at<cv::Vec3b>(h,w)[1]=255;			
+		}
+	}
+	std::cout<<"a1\n";
+*/	
+
 	cv_bridge::CvImagePtr publish_cvimage(new cv_bridge::CvImage);
-	publish_cvimage->encoding=sensor_msgs::image_encodings::MONO8;
+	publish_cvimage->encoding=sensor_msgs::image_encodings::BGR8;
 	publish_cvimage->image=grid_map.clone();
 	pub.publish(publish_cvimage->toImageMsg());	
 }
@@ -39,9 +133,12 @@ int main(int argc,char **argv){
 	std::cout<<"ready\n";
 
 	while(ros::ok()){
-		grid.subscribe_pcl();
-		
+		grid.subscribe_depth_image();
+		grid.set_grid_map();
+//		std::cout<<"setted grid_map\n";
+		grid.publish_grid_map();
 	}
 
 	return 0;
 }
+
--- src/grid_class.h
+++ src/grid_class.h
@@ -1,6 +1,11 @@
 #include"ros/ros.h"
 #include <ros/callback_queue.h>
 #include <sensor_msgs/PointCloud2.h>
+#include<image_transport/image_transport.h>
+#include<cv_bridge/cv_bridge.h>
+#include<opencv2/highgui/highgui.hpp>
+#include<opencv2/imgproc/imgproc.hpp>
+#include<sensor_msgs/image_encodings.h>
 //#include <pcl/point_types.h>
 //#include <pcl/point_cloud.h>
 //#include <pcl_conversions/pcl_conversions.h>
@@ -8,21 +13,30 @@
 class grid_class{
 	private:
 		//pcl::PointCloud<pcl::PointXYZRGB> pcl_data;
-		pcl::PointCloud<pcl::PointXYZ> pcl_data;
+		//pcl::PointCloud<pcl::PointXYZ> pcl_data;
 		ros::NodeHandle nh_pub,nh_sub;
 		ros::Subscriber sub;
 		ros::CallbackQueue queue;
 		cv::Mat grid_map;
 		image_transport::ImageTransport it_pub;
-		
+		image_transport::Publisher pub;
+		cv_bridge::CvImagePtr cvbridge_image;
+		cv::Mat depth_image;
 		int grid_resolution;//
 		float grid_size;
 		float grid_cell_size;
+		
+		const int width=672;
+		const int height=376;
+		const float f=350.505;
 	public:
 		grid_class();
 		~grid_class();
-		void subscribe_pcl(void);
-		void pcl_callback(const sensor_msgs::PointCloud2ConstPtr& msg);
+		//void subscribe_pcl(void);
+		void subscribe_depth_image(void);
+		//void pcl_callback(const sensor_msgs::PointCloud2ConstPtr& msg);
+		void image_callback(const sensor_msgs::ImageConstPtr& msg);
 		void set_grid_map(void);
 		void publish_grid_map(void);
 };
+
