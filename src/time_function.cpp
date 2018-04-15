45cc34661d060eaaca7bfe7923bb2ccc48f83dff
Shohei-Nogami <14ej081@ms.dendai.ac.jp>
2018-04-15 10:41:41 +0900

    update

--- src/time_function.cpp
+++ src/time_function.cpp
@@ -1,22 +1,31 @@
-#include"img_prc_cls.h"
-//----time----
-	void ImageProcesser::gettime(void){
-		ros::Duration time = ros::Time::now()-start_time;
-		new_time=time.toSec();
-	}
-	void ImageProcesser::getprevtime(void){
-		prev_time=new_time;
-	}
-	void ImageProcesser::culcdt(void){
-		dt=new_time-prev_time;
-	}
-	void ImageProcesser::getimgtime(void){
-		ros::Duration time = ros::Time::now()-start_time;
-		new_img_time=time.toSec();
-	}
-	void ImageProcesser::getprevimgtime(void){
-		prev_img_time=new_img_time;
-	}
-	void ImageProcesser::culcimgdt(void){
-		img_dt=new_img_time-prev_img_time;
-	}
+#include"time_class.h"
+
+time_class::time_class()
+	:first_process_flag(true)
+{
+	start_time = ros::Time::now();
+}
+time_class::~time_class()
+{
+}
+void time_class::set_cur_time(void){
+	temp_time = ros::Time::now()-start_time;
+	cur_time=temp_time.toSec();			
+}
+void time_class::set_pre_time(void){
+	pre_time=cur_time;
+}
+void time_class::set_delta_time(void){
+	delta_time=cur_time-pre_time;
+}
+void time_class::get_delta_time(double& dt){
+	dt=delta_time;
+}
+
+
+int main(int argc,char **argv){
+	ros::init(argc,argv,"time_class_test");
+	time_class time;
+	std::cout<<"finish\n";
+	return 0;
+}
