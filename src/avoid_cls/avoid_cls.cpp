fdfd765805d3aa5020cbfdfc9fac90da04cd6746
Shohei-Nogami <14ej081@ms.dendai.ac.jp>
2018-06-10 00:54:50 +0900

    Update avoid_cls.cpp

--- src/avoid_cls/avoid_cls.cpp
+++ src/avoid_cls/avoid_cls.cpp
@@ -1,25 +1,83 @@
-#include"ros/ros.h"
-#include <ros/callback_queue.h>
-#include <sensor_msgs/PointCloud2.h>
-#include<opencv2/highgui/highgui.hpp>
-#include<opencv2/imgproc/imgproc.hpp>
-#include"obst_avoid/point3d.h"
-#include"obst_avoid/object_info.h"
-#include"obst_avoid/objects_info.h"
-#include"time_class.h"
-#include <pcl_ros/point_cloud.h>
-//#include<Eigen/Core>
-//#include<Eigen/Dense>
-#include<fstream>//file input output
+#include"avoid_cls.h"
 
-class avoid{
-	private:
-		ros::NodeHandle nh_sub,nh_pub;
+#define STATIC_OBSTACLE 0
+#define DYNAMIC_OBSTACLE 1
+#define MOVING_OBSTACLE 2
+
+avoid::avoid()
+{
+	nh_sub.setCallbackQueue(&queue);
+	sub = nh_sub.subscribe("/filted_objects_info", 1, &avoid::objects_callback, this);
+}
+avoid::~avoid()
+{
+
+}
+void avoid::subscribe_objects(void)
+{
+	queue.callOne(ros::WallDuration(1));
+}
+
+void avoid::objects_callback(const obst_avoid::filted_objects_info::ConstPtr& msg)
+{
+	obj_info.objs = msg->objs;
+}
+
+void avoid::dicriminate_obstacle(void)
+{
+	is_moving_obstacle.resize(obj_info.obj.size());
+	for (int i = 0; i < obj_info.obj.size(); i++)
+	{
+
+
+		if (obj_info.objs[i].size > 1.0*1.0 || obj_info.objs[i].size < 0.2*0.2
+			|| std::sqrt(std::pow(xh_t[i](2, 0), 2.0) + std::pow(xh_t[i](3, 0), 2.0)) < 0.1
+			)
+		{
+			is_moving_obstacle[i] = STATIC_OBSTACLE;
+		}
+		else if (std::sqrt(std::pow(obj_info.objs[i].vel.x, 2.0) + std::pow(obj_info.objs[i].vel.z, 2.0)) > 1.1
+			|| std::sqrt(std::pow(sig_xh_t[i](2, 2), 2.0) + std::pow(sig_xh_t[i](2, 0), 2.0)) < xh_t[i](2, 0) )
+		{
+			is_moving_obstacle[i] = DYNAMIC_OBSTACLE;
+		}
+		else
+		{
+			is_moving_obstacle[i] = MOVING_OBSTACLE;
+		}
 		
-		ros::Publisher pub,pub_pcl;
-		ros::Subscriber sub;
-		ros::CallbackQueue queue;
+	}
+}
+
+void avoid::culculate_equation(void)
+{
+	obj_eq_a.clear();
+	obj_eq_a.resize(obj_info.obj.size());
+
+	for (int i = 0; i < obj_eq.size(); i++)
+	{
+		obj_eq_a[i] = obj_info.obj[i].vel.z / obj_info.obj[i].vel.x;
+	}
+}
+
+float avoid::select_route(void)
+{
+	//set grid map
+	for (int i = 0; i < obj_info.obj.size(); i++)
+	{
+		vfh.set_grid_map(obj_info.objs[i].pt);
+	}
+	//select_route
+	cv::Point2f x0 = cv::Point2f(0, 0);
+	cv::Point2f xp = cv::Point2f(0, 5);
+	float theta0 = 0;
+	float w_target = 0.8;
+	float w_angle = 0.5;
+	float selected_angle = vfh.select_best_trajectory(x0, theta0, xp, w_target, w_angle);
+
+	return selected_angle;
+
+}
 
 
 
-};
