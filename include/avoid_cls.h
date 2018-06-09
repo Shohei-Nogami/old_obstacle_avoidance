e1a71b7db75a4d3d000999c415e3891a4cf6575f
Shohei-Nogami <14ej081@ms.dendai.ac.jp>
2018-06-10 00:55:17 +0900

    Add files via upload

---
+++ include/avoid_cls.h
@@ -0,0 +1,41 @@
+#include"ros/ros.h"
+#include <ros/callback_queue.h>
+#include <sensor_msgs/PointCloud2.h>
+#include<opencv2/highgui/highgui.hpp>
+#include<opencv2/imgproc/imgproc.hpp>
+#include"obst_avoid/point3d.h"
+#include"obst_avoid/object_info.h"
+#include"obst_avoid/objects_info.h"
+#include"obst_avoid/filted_objects_info.h"
+#include"obst_avoid/filted_object_info.h"
+#include"time_class.h"
+#include <pcl_ros/point_cloud.h>
+//#include<Eigen/Core>
+//#include<Eigen/Dense>
+#include<fstream>//file input output
+#include"vfh+_class.h"
+
+class avoid {
+private:
+	ros::NodeHandle nh_sub, nh_pub;
+
+	ros::Publisher pub, pub_pcl;
+	ros::Subscriber sub;
+	ros::CallbackQueue queue;
+
+	obst_avoid::filted_objects_info obj_info;
+
+	std::vector<int> is_moving_obstacle;
+	vfh_class vfh;
+
+	std::vector<float> obj_eq_a;//ŒX‚«
+
+public:
+	avoid();
+	~avoid();
+	void subscribe_objects(void);
+	void objects_callback(const obst_avoid::filted_objects_info::ConstPtr& msg);
+	void dicriminate_obstacle(void);
+	void culculate_equation(void);
+	float select_route(void);
+};
